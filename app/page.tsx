7c16a77e1665c59ccad8b90907fff1159d524120
oluxkora <olumayowa@korapay.com>
2025-12-11 18:21:32 +0100

    buuild

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <Header />
+      <Hero />
+      <About />
+      <Contact />
+      <Footer />
+    </main>
+  );
+}
